8d15f9652611a05f32259a14822ffa9e704a6c7f
Umarfarooque05 <umarfarooque05@gmail.com>
2022-04-03 13:09:00 +0530

    learning react

---
+++ introduction-to-jsx-forked/src/index.js
@@ -0,0 +1,6 @@
+var React = require("react");
+var ReactDom = require("react-dom");
+
+// ReactDom.render(what to show, where to show);
+
+ReactDom.render(<h1>Hello World!</h1>, document.getElementById("root"));
