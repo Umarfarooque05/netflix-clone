7151bc02ba7b602be08abab0f7f09c71863149cf
Umar Farooque <80164102+Umarfarooque05@users.noreply.github.com>
2022-09-27 08:08:47 -0700

    Delete introduction-to-jsx directory

--- introduction-to-jsx/src/index.js
+++
@@ -1,6 +0,0 @@
-var React = require("react");
-var ReactDom = require("react-dom");
-
-// ReactDom.render(what to show, where to show);
-
-ReactDom.render(<h1>Hello World!</h1>, document.getElementById("root"));
